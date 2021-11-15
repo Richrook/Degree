4c0884f155313596091128be3349cbe1ea09dea6
mhrwh <i072ff@yamaguchi-u.ac.jp>
2021-11-15 20:09:57 +0900

    コメント変更

--- Assets/MyGame/Scripts/UIManager.cs
+++ Assets/MyGame/Scripts/UIManager.cs
@@ -2,33 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
- 
+
 public class UIManager : MonoBehaviour
 {
     //インスペクターウィンドウからゲームオブジェクトを設定する
-    
+
     [SerializeField] GameObject OptionPanel;
- 
- 
- 
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     //BackToMenuメソッドを呼び出す
-    //     BackToMenu();
-    // }
- 
- 
-    //MenuPanelでXR-HubButtonが押されたときの処理
-    //XR-HubPanelをアクティブにする
+
+    //ゲーム画面でButtonが押されたときの処理
+    //OptionPanelをアクティブにする
     public void DisplayOption()
     {
         OptionPanel.SetActive(true);
     }
- 
- 
-    //2つのDescriptionPanelでBackButtonが押されたときの処理
-    //MenuPanelをアクティブにする
+
+    //OptionPanelでBackButtonが押されたときの処理
+    //OptionPanelを非アクティブにする
     public void HideOption()
     {
         OptionPanel.SetActive(false);
