7b7e7be819c90bca108680590e9b412eddc6429e
hiro6260000 <86338341+hiro6260000@users.noreply.github.com>
2021-12-25 15:16:51 +0900

    GameObjectの名前をStageSelectControlScriptsに変更

--- Assets/MyGame/Scripts/StageSelectControlScripts.cs
+++ Assets/MyGame/Scripts/StageSelectControlScripts.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StageSelectOptionScripts : MonoBehaviour
+public class StageSelectControlScripts : MonoBehaviour
 {
     enum State
     {
