1724d7068721ea437a96d742f0836b572e951f46
raylicola <rope.champ.109@gmail.com>
2021-12-25 18:27:14 +0900

    不要なコメントを削除

--- Assets/MyGame/Scripts/PiControlScript.cs
+++ Assets/MyGame/Scripts/PiControlScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PiControlScript : MonoBehaviour
 {
     private bool isValid = true;
-    // Start is called before the first frame update
+
     public bool getIsValid()
     {
         return isValid;
