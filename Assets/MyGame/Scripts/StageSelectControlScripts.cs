ce9bc0f031834b82b7cbddf9aab7e04663239979
hiro6260000 <86338341+hiro6260000@users.noreply.github.com>
2021-12-25 16:09:12 +0900

    feat:ステージ選択画面からオプション画面とタイトル画面への遷移をできるようにした (#54)
    * ステージセレクトからエスケープキーでオプションを開けるようにした
    
    * GameObjectの名前をStageSelectControlScriptsに変更

---
+++ Assets/MyGame/Scripts/StageSelectControlScripts.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectControlScripts : MonoBehaviour
+{
+    enum State
+    {
+        StageSelect,
+        Option
+    }
+    State state;
+
+    // ステージセレクト画面と設定画面UIの取得
+    public GameObject StageSelectUI;
+    public GameObject OptionUI;
+    
+    void Start()
+    {
+        StageSelect();
+    }
+
+    void LateUpdate()
+    {
+        switch (state)
+        {
+            case State.StageSelect:
+                // エスケープキーでオプション画面に
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    Option();
+                }
+                break;
+            
+            case State.Option:
+                // エスケープキーでステージセレクト画面に
+                if (Input.GetKeyDown(KeyCode.Escape))
+                {
+                    StageSelect();
+                }
+                break;
+        }
+    }
+    
+    void StageSelect()
+    {
+        state = State.StageSelect;
+        StageSelectUI.SetActive(true);
+        OptionUI.SetActive(false);
+        Debug.Log(state);
+    }
+
+    void Option()
+    {
+        state = State.Option;
+        StageSelectUI.SetActive(false);
+        OptionUI.SetActive(true);
+        Debug.Log(state);
+    }
+}
