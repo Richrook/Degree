33959194de533c7aa429027fff885e06c6482010
Ryutaro Matsumoto <you_0816.mi@icloud.com>
2022-01-29 16:39:05 +0900

    スクリプト修正

--- Assets/MyGame/Scripts/GameClearControlScript.cs
+++ Assets/MyGame/Scripts/GameClearControlScript.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class GameClearControlScript : MonoBehaviour
 {
-    public string ReturnSceneName;
+    private string ReturnSceneName;
 
     public void OnClickStageSelectButton()
     {
         SceneManager.LoadScene("StageSelectScene");
     }
+
     public void OnClickRetryButton()
     {
         ReturnSceneName = StageControlScript.getSceneName();
--- Assets/MyGame/Scripts/GameOverControlScript.cs
+++ Assets/MyGame/Scripts/GameOverControlScript.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class GameOverControlScript : MonoBehaviour
 {
-    public string ReturnSceneName;
+    private string ReturnSceneName;
     public void OnClickStageSelectButton()
     {
         SceneManager.LoadScene("StageSelectScene");
     }
+
     public void OnClickRetryButton()
     {
         ReturnSceneName = StageControlScript.getSceneName();
