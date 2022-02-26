26332c5f38237929c5daffde8fce37c14134efc8
hiro6260000 <86338341+hiro6260000@users.noreply.github.com>
2022-02-26 16:22:27 +0900

    指摘箇所を訂正

--- Assets/MyGame/Scripts/GoalControlScript.cs
+++ Assets/MyGame/Scripts/GoalControlScript.cs
@@ -7,16 +7,16 @@ public class GoalControlScript : MonoBehaviour
     public GameObject GoalRange; //オブジェクト
     public ParticleSystem GoalEffect; //エフェクト
     //エフェクトの誤動作防止のため一度しか実行できないようにする
-    private bool Count_for_effects = false;
+    private bool is_occurred_effect = false;
     
     public void changeGoal()
     {
         GoalRange.GetComponent<Renderer>().material.color = new Color32(97,255,0,109);
         GoalRange.GetComponent<Collider>().enabled = false;
-        if (Count_for_effects == false)
+        if (is_occurred_effect == false)
         {
             GoalEffect.Play(true);
-            Count_for_effects = true;
+            is_occurred_effect = true;
         }
     }
 }
